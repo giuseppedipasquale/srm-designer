571dcecb8589f06343d3d26597bdfc2c798eb03e
giuseppedipasquale <giuseppe-dipasquale@outlook.com>
2020-11-14 22:41:09 +0100

    Creating Motor class and test

---
+++ src/motor.py
@@ -0,0 +1,10 @@
+class Motor:
+    """
+    Class to represent a full solid rocket motor.
+    """
+    def __init__(self, chamber):
+        """
+        Constructor
+        :param chamber: chamber object
+        """
+        self._chamber = chamber
---
+++ src/test.py
@@ -0,0 +1,3 @@
+
+c1 = Chamber(10,20,4)
+print(c1.v)
