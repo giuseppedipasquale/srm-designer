ec58b9f2c0ad3e754a818b90fdccebea74ed1a59
giuseppedipasquale <giuseppe-dipasquale@outlook.com>
2020-11-14 22:54:41 +0100

    Updating motor class

--- src/motor.py
+++ src/motor.py
@@ -2,12 +2,14 @@ class Motor:
     """
     Class to represent a full solid rocket motor.
     """
+
     def __init__(self, chamber):
         """
         Constructor
         :param chamber: chamber object
         """
         self._chamber = chamber
+
     @property
     def chamber(self):
         """
