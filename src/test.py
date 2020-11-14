8b03fb6c27416555d764bea66eb7e6db29ede028
giuseppedipasquale <giuseppe-dipasquale@outlook.com>
2020-11-14 23:56:58 +0100

    Various updates

--- src/test.py
+++ src/test.py
@@ -1,12 +1,13 @@
-from chamber import Chamber
-from motor import Motor
-from nozzle import Nozzle
-from propellant import Propellant
+from src.srm.chamber import Chamber
+from src.srm.motor import Motor
+from src.srm.nozzle import Nozzle
+from src.srm.grain import Grain
+from src.srm.propellant import Propellant
 
 cha = Chamber(75, 250, 6)
 noz = Nozzle(15, 1.5)
 knsu = Propellant(198, 1.13)
-gra = Grain(69,20,120,2,knsu)
+gra = Grain(69, 20, 120, 2, knsu)
 
 m = Motor(cha, noz, gra)
 print(m.chamber.lgt)
