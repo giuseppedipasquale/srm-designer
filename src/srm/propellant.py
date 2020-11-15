b92430b00585e8b3555d27924cd63074eedec3f5
giuseppedipasquale <giuseppe-dipasquale@outlook.com>
2020-11-15 11:05:30 +0100

    Various updates

--- src/srm/propellant.py
+++ src/srm/propellant.py
@@ -3,28 +3,73 @@ class Propellant:
     Generic class to represent a propellant.
     """
 
-    def __init__(self, R, k):
+    def __init__(self, R, k, T, rho, a, n, KN_coef):
         """
         Constructor
-        :param R: gas constant [J/(kg*K)]
-        :param k:
+        :param R: Gas constant [J/(kg*K)]
+        :param k: Specific heat ratio
+        :param T: Temperature [K]
+        :param rho: Propellant solid density [g/cm^3]
+        :param a: Pressure coefficient [mm/s]
+        :param n: Pressure exponent
+        :param KN_coef: KN coefficients
         """
         self._R = R
         self._k = k
+        self._T = T
+        self._rho = rho
+        self._a = a
+        self._n = n
+        self._KN_coef = KN_coef
 
     @property
     def R(self):
         """
-        :return: gas constant
+        :return: Gas constant
         """
         return self._R
 
     @property
     def k(self):
         """
-        :return: specific heat coefficient
+        :return: Specific heat ratio
         """
         return self._k
+
+    @property
+    def T(self):
+        """
+        :return: Temperature [K]
+        """
+        return self._T
+
+    @property
+    def rho(self):
+        """
+        :return: Propellant solid density [g/cm^3]
+        """
+        return self._rho
+
+    @property
+    def a(self):
+        """
+        :return: Pressure coefficient [mm/s]
+        """
+        return self._a
+
+    @property
+    def n(self):
+        """
+        :return: Pressure exponent
+        """
+        return self._n
+
+    @property
+    def KN_coef(self):
+        """
+        :return: KN coefficients
+        """
+        return self._KN_coef
 
 
 class KNSU(Propellant):
@@ -39,6 +84,4 @@ class KNSU(Propellant):
         self._rho = 1.795
         self._a = 8.26
         self._n = 0.319
-        self._A = 32.954
-        self._B = 44.108
-        self._C = -1.1025
+        self._KN_coef = [32.954, 44.108, -1.1025]
--- src/test.py
+++ src/test.py
@@ -2,15 +2,20 @@ from src.srm.chamber import Chamber
 from src.srm.motor import Motor
 from src.srm.nozzle import Nozzle
 from src.srm.grain import Grain
-from src.srm.propellant import Propellant
+from src.srm.propellant import *
 
-cha = Chamber(75, 250, 6)
-noz = Nozzle(15, 1.5)
-knsu = Propellant(198, 1.13)
-gra = Grain(knsu, 69, 20, 120, 2, 1, 0, 0)
+# cha = Chamber(75, 250, 6)
+# noz = Nozzle(15, 1.5)
+# knsu = Propellant(198, 1.13)
+# gra = Grain(knsu, 69, 20, 120, 2, 1, 0, 0)
 
-m = Motor(cha, noz, gra)
-print(m.chamber.lgt)
-print(m.nozzle.dt)
-print(m.grain.lgt)
-# todo test all changes
+knsu = KNSU()
+print(knsu.KN_coef)
+print(knsu.n)
+print(knsu.k)
+
+# m = Motor(cha, noz, gra)
+# print(m.chamber.lgt)
+# print(m.nozzle.dt)
+# print(m.grain.lgt)
+# # todo test all changes
