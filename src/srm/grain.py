f186c78b569d74f83befe03a719726d4f2095d1a
giuseppedipasquale <giuseppe-dipasquale@outlook.com>
2020-11-15 11:07:16 +0100

    Various updates

--- src/srm/grain.py
+++ src/srm/grain.py
@@ -3,10 +3,10 @@ class Grain:
     Class to represent a grain or set of propellant grains.
     """
 
-    def __init__(self, prop, d_ext, d_int, lgt, n, int_inhibit, ext_inhibit, lat_inhibit):
+    def __init__(self, propellant, d_ext, d_int, lgt, n, int_inhibit, ext_inhibit, lat_inhibit):
         """
         Constructor
-        :param prop: Propellant
+        :param propellant: Propellant
         :param d_ext: External diameter [mm]
         :param d_int: Internal diameter [mm]
         :param lgt: Length of single grain [mm]
@@ -19,7 +19,7 @@ class Grain:
         self._d_int = d_int
         self._lgt = lgt
         self._n = n
-        self._prop = prop
+        self._prop = propellant
         self._int_inhibit = int_inhibit
         self._ext_inhibit = ext_inhibit
         self._lat_inhibit = lat_inhibit
--- src/test.py
+++ src/test.py
@@ -4,18 +4,14 @@ from src.srm.nozzle import Nozzle
 from src.srm.grain import Grain
 from src.srm.propellant import *
 
-# cha = Chamber(75, 250, 6)
-# noz = Nozzle(15, 1.5)
-# knsu = Propellant(198, 1.13)
-# gra = Grain(knsu, 69, 20, 120, 2, 1, 0, 0)
-
+cha = Chamber(75, 250, 6)
+noz = Nozzle(15, 1.5)
 knsu = KNSU()
-print(knsu.KN_coef)
-print(knsu.n)
-print(knsu.k)
+gra = Grain(knsu, 69, 20, 120, 2, 1, 0, 0)
 
-# m = Motor(cha, noz, gra)
-# print(m.chamber.lgt)
-# print(m.nozzle.dt)
-# print(m.grain.lgt)
-# # todo test all changes
+
+m = Motor(cha, noz, gra)
+print(m.chamber.lgt)
+print(m.nozzle.dt)
+print(m.grain.prop.)
+# todo test all changes
